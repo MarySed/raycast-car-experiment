cdc20d4244188280b57c7d634f7e63e504806207
Mary <37863665+MarySed@users.noreply.github.com>
2021-09-28 21:42:21 +0900

    added new car model and updated to fit

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,17 +1,17 @@
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
-import { useRaycastVehicle } from '@react-three/cannon'
-import { useControls } from './utils/useControls'
-import Beetle from './Beetle'
-import Wheel from './Wheel'
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useRaycastVehicle } from '@react-three/cannon';
+import { useControls } from './utils/useControls';
+import Drifter from './Drifter';
+import Wheel from './Wheel';
 
-function Vehicle({ radius = 0.7, width = 1.2, height = -0.04, front = 1.3, back = -1.15, steer = 0.75, force = 2000, maxBrake = 1e5, ...props }) {
-  const chassis = useRef()
-  const wheel1 = useRef()
-  const wheel2 = useRef()
-  const wheel3 = useRef()
-  const wheel4 = useRef()
-  const controls = useControls()
+function Vehicle({ radius = 0.7, width = 1.2, height = 0.3, front = 1.3, back = -1.15, steer = 0.75, force = 2000, maxBrake = 1e5, ...props }) {
+  const chassis = useRef();
+  const wheel1 = useRef();
+  const wheel2 = useRef();
+  const wheel3 = useRef();
+  const wheel4 = useRef();
+  const controls = useControls();
 
   const wheelInfo = {
     radius,
@@ -27,12 +27,12 @@ function Vehicle({ radius = 0.7, width = 1.2, height = -0.04, front = 1.3, back 
     useCustomSlidingRotationalSpeed: true,
     customSlidingRotationalSpeed: -30,
     frictionSlip: 2
-  }
+  };
 
-  const wheelInfo1 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [-width / 2, height, front] }
-  const wheelInfo2 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [width / 2, height, front] }
-  const wheelInfo3 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [-width / 2, height, back] }
-  const wheelInfo4 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [width / 2, height, back] }
+  const wheelInfo1 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [-width / 1.5, height, front] };
+  const wheelInfo2 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [width / 1.5, height, front] };
+  const wheelInfo3 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [-width / 1.5, height, back] };
+  const wheelInfo4 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [width / 1.5, height, back] };
 
   const [vehicle, api] = useRaycastVehicle(() => ({
     chassisBody: chassis,
@@ -41,30 +41,30 @@ function Vehicle({ radius = 0.7, width = 1.2, height = -0.04, front = 1.3, back 
     indexForwardAxis: 2,
     indexRightAxis: 0,
     indexUpAxis: 1
-  }))
+  }));
 
   useFrame(() => {
-    const { forward, backward, left, right, brake, reset } = controls.current
-    for (let e = 2; e < 4; e++) api.applyEngineForce(forward || backward ? force * (forward && !backward ? -1 : 1) : 0, 2)
-    for (let s = 0; s < 2; s++) api.setSteeringValue(left || right ? steer * (left && !right ? 1 : -1) : 0, s)
-    for (let b = 2; b < 4; b++) api.setBrake(brake ? maxBrake : 0, b)
+    const { forward, backward, left, right, brake, reset } = controls.current;
+    for (let e = 2; e < 4; e++) api.applyEngineForce(forward || backward ? force * (forward && !backward ? -1 : 1) : 0, 2);
+    for (let s = 0; s < 2; s++) api.setSteeringValue(left || right ? steer * (left && !right ? 1 : -1) : 0, s);
+    for (let b = 2; b < 4; b++) api.setBrake(brake ? maxBrake : 0, b);
     if (reset) {
-      chassis.current.api.position.set(0, 0.5, 0)
-      chassis.current.api.velocity.set(0, 0, 0)
-      chassis.current.api.angularVelocity.set(0, 0.5, 0)
-      chassis.current.api.rotation.set(0, -Math.PI / 4, 0)
+      chassis.current.api.position.set(0, 0.5, 0);
+      chassis.current.api.velocity.set(0, 0, 0);
+      chassis.current.api.angularVelocity.set(0, 0.5, 0);
+      chassis.current.api.rotation.set(0, -Math.PI / 4, 0);
     }
-  })
+  });
 
   return (
-    <group ref={vehicle} position={[0, -0.4, 0]}>
-      <Beetle ref={chassis} rotation={props.rotation} position={props.position} angularVelocity={props.angularVelocity} />
+    <group ref={vehicle} position={[0, -0.3, 0]}>
+      <Drifter ref={chassis} rotation={props.rotation} position={props.position} angularVelocity={props.angularVelocity} />
       <Wheel ref={wheel1} radius={radius} leftSide />
       <Wheel ref={wheel2} radius={radius} />
       <Wheel ref={wheel3} radius={radius} leftSide />
       <Wheel ref={wheel4} radius={radius} />
     </group>
-  )
+  );
 }
 
-export default Vehicle
+export default Vehicle;
